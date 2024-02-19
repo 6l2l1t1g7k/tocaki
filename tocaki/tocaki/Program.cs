eca684310a4f7bd0836033430db3d98c24216986
Sofft <Sofft@LAPTOP-TIS53PIK>
2024-02-19 14:40:07 +0100

    boucle pour les commandes

--- tocaki/tocaki/Program.cs
+++ tocaki/tocaki/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool exit = false;
+string userInput;
+
+while (!exit)
+{
+    Console.WriteLine("");
+
+    Console.Write("Commande:");
+    userInput = Console.ReadLine() ?? string.Empty;
+
+    switch (userInput)
+    {
+        case "0":
+            exit = true;
+            break;
+        case "exit":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Commande non reconnue.");
+            break;
+    }
+}
+
